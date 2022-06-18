60bdb38afde11516b7bae8881efa3a9f7958a8b4
Alan Blanchet <alan@alan-blanchet.fr>
2022-06-18 21:01:52 +0200

    [FIX]Lib update

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -11,10 +10,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-=======
-import { render } from 'react-dom';
-
-import { App } from './components/App';
-
-render(<App />, document.getElementById('root'));
->>>>>>> a62b489 ([DEV]Added the utils from old lib)
