a987616e444e1e50b14bc82bd6825db71da310c7
AlanBlanchetNeovision <alan.blanchet@neovision.fr>
2022-12-07 14:06:45 +0100

    [FIX]Can set boolean value if needed
    Enables giving value to setState

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -2,5 +2,5 @@ import { useState } from 'react';
 
 export const useTrigger = (def: boolean = false) => {
   const [trigger, setTrigger] = useState(def);
-  return [trigger, () => setTrigger(!trigger)];
+  return [trigger, (b: boolean = !trigger) => setTrigger(b)];
 };
