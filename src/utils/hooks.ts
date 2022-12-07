b7bc257169306f541e0420aa03c586bb3b27f998
AlanBlanchetNeovision <alan.blanchet@neovision.fr>
2022-12-07 16:00:24 +0100

    [FIX]Previous fix didn't work out as expected
    The fix should now take effect

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -2,8 +2,6 @@ import { useState } from 'react';
 
 export const useTrigger = (def: boolean = false) => {
   const [trigger, setTrigger] = useState(def);
-  return [trigger, (b: boolean = !trigger) => setTrigger(b)] as [
-    typeof trigger,
-    typeof setTrigger,
-  ];
+  const f = (b: boolean = !trigger) => setTrigger(b);
+  return [trigger, f] as [typeof trigger, typeof f];
 };
